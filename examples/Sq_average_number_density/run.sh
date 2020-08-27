25a180ff88930987143e80d4b598af83727a751f
Jesse Gatten McDaniel <jmcdaniel43@login-s1.pace.gatech.edu>
2020-08-26 20:26:40 -0400

    updated example in 'examples/Sq_average_number_density/' to work with last commit of source code that requires a parameter file as input

--- examples/Sq_average_number_density/run.sh
+++ examples/Sq_average_number_density/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-../../Sq_Average_FFT_source/main_structure nvt_300K.gro traj.gro ../../atomic_form_factors/ > Sq_number.out
+# calculate scattering structure factor with PME approach...
+../../Sq_Average_FFT_source/main_structure nvt_300K.gro traj.gro ../../atomic_form_factors/ calculation_parameters.pmt > Sq_number.out
+
+# now use a little perl script to average the scattering and plot isotropic scattering as a function of wavevector magnitude
+../../perl_scripts/average_Sq.pl Sq_number.out > Sq_mag
